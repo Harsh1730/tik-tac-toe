c49aa755ad6e6f492b99a65fe055faaf3c83e5ab
Harsh Singh Saini <harshsaini8789@gmail.com>
2025-04-15 23:11:00 +0530

    Update java.js

--- java.js
+++ java.js
@@ -9,7 +9,7 @@ const win = [
   [3, 4, 5],
   [6, 7, 8],
 ];
-
+document.querySelector(".turn").innerHTML="Harsh";
 function har() {
   let tik = document.querySelectorAll(".tik");
   tik.forEach((tik) => (tik.innerHTML = ""));
